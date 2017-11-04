21475d1b0c11cd682ce0e311ec189375eb366ddb
HesslerN <nicolehessler2021@u.northwestern.edu>
2017-11-04 14:42:05 -0500

    made a struct for musicDatas. Also comments

--- music-map/ViewController.swift
+++ music-map/ViewController.swift
@@ -13,38 +13,58 @@ import CoreLocation
 class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     
-    var trackLocationDict = [MPMediaItem:CLLocationCoordinate2D]()
+    //set up what a musicData is (spoiler: it's time + song + location)
+    struct MusicData {
+        var time : Date?
+        var song : MPMediaItem?
+        var location : CLLocationCoordinate2D?
+        init(){
+            time = nil
+            song  = nil
+            location = nil
+        }
+        init(time nowTime : Date?, song nowSong : MPMediaItem?, location nowLocation : CLLocationCoordinate2D?){
+            time = nowTime
+            song = nowSong
+            location = nowLocation
+        }
+    }
+    var musicDatas = [MusicData]()
+    
     let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
+        //when the view loads, begin checking for when the song changes
         super.viewDidLoad()
-
         MPMusicPlayerController.systemMusicPlayer.beginGeneratingPlaybackNotifications()
         NotificationCenter.default.addObserver(self, selector: #selector(trackChanged), name: .MPMusicPlayerControllerNowPlayingItemDidChange, object: nil)
+        
+        //when the view loads, begin an AR session
+        
     }
     
+    //when the view appears, get location authorization
     override func viewDidAppear(_ animated: Bool) {
         switch CLLocationManager.authorizationStatus() {
             case .notDetermined:
                 // Request when-in-use authorization initially
                 locationManager.requestWhenInUseAuthorization()
                 break
-            
             case .restricted, .denied:
                 print("User denied location request")
                 break
-            
             case .authorizedWhenInUse, .authorizedAlways:
                 break
             }
     }
-
+    
+    //when the track changes, record the time, song, and location
     @objc func trackChanged() {
         let nowPlaying = MPMusicPlayerController.systemMusicPlayer.nowPlayingItem
-        
         let coordinates = locationManager.location?.coordinate
         if let nowPlayingItem = nowPlaying{
-            trackLocationDict[nowPlayingItem] = coordinates
+            let data = MusicData(time : Date(), song : nowPlayingItem, location : coordinates)
+            musicDatas.append(data)
         }
     }
 }
